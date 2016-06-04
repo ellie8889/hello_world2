dffcfd97ae45d9f50cd22e8642276d83aece2e96
kcm <t101598049@gmail.com>
2016-06-04 14:05:49 +0800

    ba_swap

--- test.c
+++ test.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+#include <stdio.h>
 int main()
 {
     int i;
-    printf("this is git c code\n");
-
-
+    printf("this is git c code and swap_a&b\n");
 return 0;
 }
-
-
+void foo_b()
+{
+    int b;
+}
+void f00_a()
+{
+    int a;
+}
 
